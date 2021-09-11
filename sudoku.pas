2e98ab9a33309ab629006d90ec341f1fdcb82469
haruki-taka8 <stevefestl@gmail.com>
2021-09-11 11:48:13 +0800

    Preliminary support for hidden triple

--- sudoku.pas
+++ sudoku.pas
@@ -5,7 +5,8 @@ uses
     auxiliary    in 'Modules\General\auxiliary.pas',
     io           in 'Modules\General\io.pas',
     endgame      in 'Modules\General\endgame.pas',      
-    GetHint      in 'Modules\General\getHint.pas', 
+    GetHint      in 'Modules\General\getHint.pas',
+    HiddenTriple in 'Modules\Solving\hiddenTriple.pas',
     NakedSingle  in 'Modules\Solving\nakedSingle.pas',
     NakedPair    in 'Modules\Solving\nakedPair.pas',
     HiddenSingle in 'Modules\Solving\hiddenSingle.pas',
@@ -18,26 +19,31 @@ uses
 var
     // Global variables, use with caution
     grid    : TIntegerGrid;
-    oldGrid : TIntegerGrid;
+    // oldGrid : TIntegerGrid;
     hint    : TStringGrid;
+    i : integer;
 
 begin
     ReadGrid(grid);
     GetHint.GetHint(grid, hint);
+    WriteHint(hint);
 
     // Solving loop
-    while not IsSolved(grid) do
+    // while not IsSolved(grid) do
+    i := 0;
+    while i < 16 do
     begin
-        oldGrid := grid;
+        // oldGrid := grid;
 
         // Solve
-        // Took me an hour to realize the Unit.Interface convention
+        // Took me an hour to realize the Unit.Implementation convention
         // ALL ONLINE DOCUMENTATIONS ARE COMPLETELY WRONG!
         GetHint.RemoveSolved(grid, hint);
         
         HiddenPair.RemoveHint(hint);
         NakedPair.RemoveHint(hint);
         NakedTriple.RemoveHint(hint);
+        HiddenTriple.RemoveHint(hint);
         ClaimingPair.RemoveHint(hint);
         PointingPair.RemoveHint(hint);
         
@@ -48,16 +54,21 @@ begin
         // Repeatition Detection
         if VERBOSE then writeln;
         
-        if IsRepeated(grid, oldGrid) then
-        begin
-            WriteHint(hint);
-            writeln;
-            writeln('REPETITION DETECTED; BAILING!');
-            break;
-        end;
-            
+        // if IsRepeated(grid, oldGrid) then
+        // begin
+        //     WriteHint(hint);
+        //     writeln;
+        //     writeln('REPETITION DETECTED; BAILING!');
+        //     break;
+        // end;
+        if IsSolved(grid) then break;
+
+        i := i + 1;
     end;
     
     WriteGrid(grid);
-    if IsSolved(grid) then writeln('SOLVED!');
+    if IsSolved(grid) then
+        writeln('SOLVED! - ', i, ' steps used!')
+    else
+        WriteHint(hint);
 end.
